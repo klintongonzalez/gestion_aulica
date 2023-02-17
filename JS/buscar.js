ec5bd150a69790b00777d3abae6bb3bf1a9bdd9a
klintongonzalez <125670539+klintongonzalez@users.noreply.github.com>
2023-02-17 09:03:06 -0500

    Delete buscar.js

--- JS/buscar.js
+++
@@ -1,8 +0,0 @@
-function buscar() {
-    var search = prompt("Termino de busqueda:");
-    if (search == null || search == "") {
-        alert("Busqueda Cancelada");
-    } else {
-        find(search)
-    }
-}
